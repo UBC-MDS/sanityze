c46ecf10661ac2244633851f6acbdd5fe9f441de
Jonah Hamilton <jonah.hamilton@gmail.com>
2023-01-18 10:25:11 -0800

    adding text and fixing test failure

--- tests/test_creditcard_Spotter.py
+++ tests/test_creditcard_Spotter.py
@@ -12,16 +12,19 @@ fake_cc = {"VISA" : "4556129404313766",
            "MASTERCARD" : "5567554868135971",  
            "AMEX" : "345160678082328", 
            "DINERS" : "3013130458900846", 
-           "DISCOVER" :"6011087735246416",
+           "DISCOVER" : "6011087735246416",
            "JCB" : "3538159804477445"
            }
 
 # test string for credit card md5 hashes
-cc_numbers = "VISA, 4929688015693122"
-credit_hashes = "VISA, 34c2171639b834dce4b1c0183a91d427"
+cc_number = "VISA, 4929688015693122"
+credit_hash = "VISA, 34c2171639b834dce4b1c0183a91d427"
 
 # strings of equal length to credit cards
 number_string = "5628404238239405, 5673289472024660, 8709, 356785, 1111111111111111"
+
+# test string for full credit card info
+full_cc_info = "VISA, 4916363769587210, 10/2023, 992"
 
 # check if uid corect UID is returned 
 def test_creditcard_spotter_uid():
@@ -42,6 +45,10 @@ def test_creditcard_spotter_cc_match():
     for cc in fake_cc.values():
         assert "CREDITCARD" == cc_Spotter_no_hash.process(cc)
 
+# check if cc number in a string is replaced
+def test_creditcard_spotter_full_cc():
+    assert "VISA, CREDITCARD, 10/2023, 992" == cc_Spotter_no_hash.process(full_cc_info)
+
 # check correct hashes are replaced
 def test_creditcard_spotter_hash():
-    assert credit_hashes == cc_Spotter_hash.process(cc_numbers)
+    assert credit_hash == cc_Spotter_hash.process(cc_number)
